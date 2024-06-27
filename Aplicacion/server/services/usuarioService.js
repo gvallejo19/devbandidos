5a5f208a2d899192ab52e2d77651047e039ce48b
Christian Alexander Espín Díaz <75314331+ChristianPredator@users.noreply.github.com>
2024-06-27 11:25:15 -0500

    cambios
    usuario

--- Aplicacion/server/services/usuarioService.js
+++ Aplicacion/server/services/usuarioService.js
@@ -1,97 +1,139 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { Usuario } from "../models/usuario.js";
-
-// Definimos nuestra clave secreta aquí (debe ser robusta y segura)
-const SECRET_KEY = "your_secret_key_here";
+import { Usuarios } from "../models/usuarios.js";
+import { Administradores } from "../models/administradores.js";
+import { Nutricionista } from "../models/nutricionistas.js";
+import { createAdmin, deleteAdmin, updateAdmin } from "../utilities/utilitiesAdministrador.js";
+import { createNutricionista, deleteNutricionista, updateNutricionista } from "../utilities/utilitiesNutricionista.js";
 
 class UsuariosService {
   async getAllUsuarios() {
     console.log('Fetching all users...');
-    const Usuarios = await Usuario.findAll();
-    console.log('Fetched users:', Usuarios);
-    return Usuarios;
+    const usuarios = await Usuarios.findAll();
+    console.log('Fetched users:', usuarios);
+
+    const usuariosConRelaciones = await Promise.all(
+      usuarios.map(async (usuario) => {
+        let include = [];
+        switch (usuario.tipo_usuario) {
+          case 'administrador':
+            include.push({ model: Administradores, as: 'administrador', required: true });
+            break;
+          case 'nutricionista':
+            include.push({ model: Nutricionista, as: 'nutricionista', required: true });
+            break;
+          default:
+            break;
+        }
+        const usuarioConRelacion = await Usuarios.findByPk(usuario.id_usuario, { include });
+        return usuarioConRelacion;
+      })
+    );
+    return usuariosConRelaciones;
   }
 
   async getUsuarioById(id) {
     console.log(`Fetching user with ID: ${id}`);
-    const Usuario = await Usuario.findByPk(id);
-    if (!Usuario) {
-      console.log(`user with ID: ${id} not found`);
+    const usuario = await Usuarios.findByPk(id, {
+      include: [
+        { model: Administradores, as: 'administrador', required: false },
+        { model: Nutricionista, as: 'nutricionista', required: false }
+      ]
+    });
+    if (!usuario) {
+      console.log(`User with ID: ${id} not found`);
     } else {
-      console.log('Fetched user:', Usuario);
+      console.log('Fetched user:', usuario);
     }
-    return Usuario;
+    return usuario;
   }
 
-  async createUsuario(UsuarioData) {
-    console.log('Creating new user with data:', UsuarioData);
-    const hashedPassword = await bcrypt.hash(UsuarioData.contrasena, 10);  // Hashing the password
-    const newUsuario = await Usuario.create({ ...UsuarioData, contrasena: hashedPassword });
+  async getUsuariosByTipo(tipo_usuario) {
+    console.log(`Fetching users with type: ${tipo_usuario}`);
+    let include = [];
+    switch (tipo_usuario) {
+      case 'administrador':
+        include.push({ model: Administradores, as: 'administrador', required: true });
+        break;
+      case 'nutricionista':
+        include.push({ model: Nutricionista, as: 'nutricionista', required: true });
+        break;
+      default:
+        throw new Error(`Tipo de usuario no reconocido: ${tipo_usuario}`);
+    }
+    const usuarios = await Usuarios.findAll({ where: { tipo_usuario }, include });
+    console.log('Fetched users by type:', usuarios);
+    return usuarios;
+  }
 
+  async createUsuario(usuarioData, transaction) {
+    console.log('Creating new user with data:', usuarioData);
+    const newUsuario = await Usuarios.create(usuarioData, { transaction });
+
+    switch (usuarioData.tipo_usuario) {
+      case 'administrador':
+        await createAdmin({ id_usuario: newUsuario.id_usuario }, transaction);
+        console.log(`Created admin for user ID: ${newUsuario.id_usuario}`);
+        break;
+      case 'nutricionista':
+        await createNutricionista({ id_usuario: newUsuario.id_usuario, especialidad: usuarioData.especialidad }, transaction);
+        console.log(`Created nutritionist for user ID: ${newUsuario.id_usuario}`);
+        break;
+      default:
+        break;
+    }
     console.log('Created user:', newUsuario);
     return newUsuario;
   }
 
-  async updateUsuario(id, UsuarioData) {
+  async updateUsuario(id, usuarioData, transaction) {
     console.log(`Updating user with ID: ${id}`);
-    const Usuario = await Usuario.findByPk(id);
+    const usuario = await Usuarios.findByPk(id, { transaction });
 
-    if (!Usuario) {
-      throw new Error('user not found');
+    if (!usuario) {
+      throw new Error('Usuario not found');
     }
 
-    await Usuario.update(UsuarioData);
-    console.log('Updated user:', Usuario);
-    return Usuario;
+    await usuario.update(usuarioData, { transaction });
+
+    switch (usuarioData.tipo_usuario) {
+      case 'administrador':
+        await updateAdmin(usuario.id_usuario, transaction);
+        console.log(`Updated admin for user ID: ${id}`);
+        break;
+      case 'nutricionista':
+        await updateNutricionista(usuario.id_usuario, usuarioData.especialista, transaction);
+        console.log(`Updated nutritionist for user ID: ${id}`);
+        break;
+      default:
+        break;
+    }
+    console.log('Updated user:', usuario);
+    return usuario;
   }
 
-  async deleteUsuario(id) {
+  async deleteUsuario(id, transaction) {
     console.log(`Deleting user with ID: ${id}`);
-    const Usuario = await Usuario.findByPk(id);
+    const usuario = await Usuarios.findByPk(id, { transaction });
 
-    if (!Usuario) {
-      throw new Error('user not found');
+    if (!usuario) {
+      throw new Error('Usuario not found');
     }
 
-    await Usuario.destroy();
-    console.log(`user with ID: ${id} deleted`);
-    return { message: 'user deleted' };
-  }
-
-  async loginUsuario(email, contrasena) {
-    console.log(`Logging in user with email: ${email}`);
-    const Usuario = await Usuario.findOne({ where: { email } });
-    
-    if (!Usuario) {
-      throw new Error('user not found');
+    switch (usuario.tipo_usuario) {
+      case 'administrador':
+        await deleteAdmin(usuario.id_usuario, transaction);
+        console.log(`Deleted admin for user ID: ${id}`);
+        break;
+      case 'nutricionista':
+        await deleteNutricionista(usuario.id_usuario, transaction);
+        console.log(`Deleted nutritionist for user ID: ${id}`);
+        break;
+      default:
+        break;
     }
 
-    const isPasswordValid = await bcrypt.compare(contrasena, Usuario.contrasena);
-    
-    if (!isPasswordValid) {
-      throw new Error('Incorrect password');
-    }
-
-    const token = jwt.sign({ id: Usuario.id_Usuario, tipo_usuario: Usuario.tipo_usuario }, SECRET_KEY, { expiresIn: '1h' });
-    
-    console.log('Login successful, token generated:', token);
-    
-    return { message: 'Login successful', token };
-  }
-
-  async changePasswordUsuario(id, newPassword) {
-    console.log(`Changing password for user with ID: ${id}`);
-    const Usuario = await Usuario.findByPk(id);
-
-    if (!Usuario) {
-      throw new Error('user not found');
-    }
-
-    const hashedNewPassword = await bcrypt.hash(newPassword, 10);
-    await Usuario.update({ contrasena: hashedNewPassword });
-    
-    return { message: 'Password updated successfully' };
+    await usuario.destroy({ transaction });
+    console.log(`User with ID: ${id} deleted`);
+    return { message: 'Usuario eliminado' };
   }
 }
 
